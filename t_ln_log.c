a263e62bca97db93da51e30868deceb14a6edd26
Matthias Andree <matthias.andree@gmx.de>
2001-01-22 13:44:31 +0100

    Snapshot 2.0b8_ma2

--- t_ln_log.c
+++ t_ln_log.c
@@ -1,10 +1,11 @@
 #include "ln_log.h"
 #include <errno.h>
 
-int verbose=9;
+int verbose = 9;
 
-main() {
-    errno = 4; 
+main()
+{
+    errno = 4;
     ln_log_prt(LNLOG_NOTICE, "test");
     ln_log_prt(LNLOG_NOTICE, "test %%");
     ln_log_prt(LNLOG_NOTICE, "test %d", 4);
