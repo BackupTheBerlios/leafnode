a76b65d3ff60518d483e551cce08fcc283e3f461
Matthias Andree <matthias.andree@gmx.de>
2001-06-28 00:03:44 +0100

    Enhance to support building dependencies outside $(srcdir).

--- makedepend.sh
+++ makedepend.sh
@@ -7,7 +7,8 @@
 
 # arguments: 
 # $1: Makefile to attach dependencies to
-# $2...: Files to generate dependencies from
+# $2...: gcc arguments until --
+# $...: Files to generate dependencies from
 
 set -e
 if test "x$1" = "x" ; then exit 1 ; fi
@@ -17,5 +18,8 @@ shift
 sed <"$f" >"${f}.mkdep" -e '/^# DO NOT DELETE/,$ d'
 echo >>"${f}.mkdep" '# DO NOT DELETE'
 echo >>"${f}.mkdep" ''
-gcc -MM >>"${f}.mkdep" "$@"
+gccargs=""
+while test "x$1" != "x--" ; do gccargs="$gccargs $1" ; shift ; done
+shift
+${CC-gcc} $gccargs -MM >>"${f}.mkdep" "$@"
 mv -b "${f}.mkdep" "$f"
