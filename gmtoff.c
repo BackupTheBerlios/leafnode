2c4e487106bd2eb89a705bfaef9a23ec712ac05c
Matthias Andree <matthias.andree@gmx.de>
2001-12-18 01:15:48 +0100

    New file, to carry a new calculation routine for the GMT offset. Relies New file, to carry a new calculation routine for the GMT offset. Relies on time.h and libc to parse the time zone staff. No more environment fiddling.

---
+++ gmtoff.c
@@ -0,0 +1,21 @@
+/** gmtoff.c - get time zone offset in seconds
+ * (C) 2001 by Matthias Andree
+ */
+
+#include "leafnode.h"
+
+#include <time.h>
+
+/** Returns number of seconds the local time zone is ahead (east) of UTC */
+time_t
+gmtoff(void)
+{
+    time_t t1, t2;
+    struct tm *t;
+
+    t1 = time(0);
+    t = gmtime(&t1);
+    t2 = mktime(t);
+
+    return t1 - t2;
+}
