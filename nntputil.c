15aa8b6dcba1178de0770d7039b39f037fc772cc
Matthias Andree <matthias.andree@gmx.de>
2002-01-05 22:50:33 +0100

    Support IPv6 (NNTP client) if the OS provides getaddrinfo(3). Support IPv6 (NNTP client) if the OS provides getaddrinfo(3). Add some lclint annotations. Fix some bugs, add some assert().

--- nntputil.c
+++ nntputil.c
@@ -7,24 +7,28 @@
 #include "ln_log.h"
 #include "h_error.h"
 #include "attributes.h"
+#include "masock.h"
+#include "format.h"
+#include "get.h"
+
+#include <assert.h>
 #include <fcntl.h>
-#include <sys/uio.h>
 #include <sys/param.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <netdb.h>
 #include <setjmp.h>
 #include <stdarg.h>
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 #include <dirent.h>
@@ -33,25 +37,20 @@ int stat_is_evil;
 long sendbuf;
 extern struct serverlist *current_server;
 char last_command[1025];
-FILE *nntpin = NULL;
+FILE *nntpin  = NULL;
 FILE *nntpout = NULL;
 int authenticated;
-static jmp_buf timeout;
-static void
-timer(int sig)
-{
-    (void)sig;
-    longjmp(timeout, 1);
-}
 
 /*
  * Authenticate ourselves at a remote server.
  * Returns TRUE if authentication succeeds, FALSE if it does not.
  */
-int
+bool
 authenticate(void)
 {
     int d, reply;
+
+    assert(nntpout != NULL);
 
     if (!current_server) {
 	ln_log(LNLOG_SERR, LNLOG_CTOP, "authenticate: no server selected");
@@ -72,7 +71,7 @@ authenticate(void)
     if (debug & DEBUG_NNTP)
 	ln_log(LNLOG_SDEBUG, LNLOG_CSERVER,
 	       ">AUTHINFO USER %s", current_server->username);
-    fflush(nntpout);
+    if (fflush(nntpout)) return FALSE;
     reply = nntpreply();
     debug = d;
     if (reply == 281) {
@@ -94,7 +93,7 @@ authenticate(void)
     if (debug & DEBUG_NNTP)
 	ln_log(LNLOG_SDEBUG, LNLOG_CSERVER,
 	       ">AUTHINFO PASS [password not shown]");
-    fflush(nntpout);
+    if (fflush(nntpout)) return FALSE;
     reply = nntpreply();
     debug = d;
     if (reply != 281) {
@@ -109,16 +108,18 @@ authenticate(void)
  * decode an NNTP reply number
  * reads a line from the server and returns an integer
  *
- * -1 is used to mean "protocol error"
+ * -1 is used to flag "protocol error"
  *
  * the text returned is discarded
  */
 int
-newnntpreply(char **resline)
+newnntpreply(/*@out@*/ char **resline)
 {
     char *response;
     int r = 0;
-    int c = 1;
+    bool c = TRUE;
+
+    assert(nntpin != NULL);
 
     while (c) {
 	response = getaline(nntpin);
@@ -133,9 +134,13 @@ newnntpreply(char **resline)
 	    && ((response[3] == ' ')
 		|| (response[3] == '\0')
 		|| (response[3] == '-'))) {
+	    long rli;
 	    int rl;
 
-	    rl = strtol(response, NULL, 10);
+	    if (0 == get_long(response, &rli) ||
+		rli > INT_MAX || rli < INT_MIN)
+		r = -1;
+	    rl = (int)rli;
 	    if (r > 0 && r != rl)
 		r = -1;		/* protocol error */
 	    else
@@ -161,6 +166,153 @@ nntpreply(void)
 
 extern struct state _res;
 
+static int
+any_connect(const int family, const int socktype, const int protocol,
+	    const struct sockaddr *sa, socklen_t addrlen,
+	    /*@exposed@*/ const char ** const errcause)
+/*@modifies *errcause@ */
+{
+    char *as;
+    int sock;
+
+    as = masock_sa2addr(sa);
+    ln_log(LNLOG_SINFO, LNLOG_CSERVER,
+	   "  trying address %s port %ld...",
+	   as ? as : "(unknown)", masock_sa2port(sa));
+
+    sock = socket(family, socktype, protocol);
+    if (sock < 0) {
+	ln_log(LNLOG_SINFO, LNLOG_CSERVER, "  cannot create socket: %m");
+	*errcause = "cannot create socket";
+    } else {
+	if (connect(sock, sa, addrlen) < 0) {
+	    int e = errno;
+	    ln_log(LNLOG_SINFO, LNLOG_CSERVER, "  cannot connect: %m");
+	    (void)close(sock);
+	    *errcause = "cannot connect";
+	    errno = e;
+	    sock = -1;
+	} else {
+	    ln_log(LNLOG_SINFO, LNLOG_CSERVER,
+		   "  connected: address %s port %ld.",
+		   as ? as : "(unknown)", masock_sa2port(sa));
+	}
+    }
+    if (as)
+	free(as);
+    return sock;
+}
+
+/** Resolve host name and connect to it.
+ * \return -1 for trouble
+ */
+static int
+tcp_connect(const char *const nodename, const char *const service,
+	    int address_family)
+{
+    const char *errcause;
+    int sock;
+
+#ifdef HAVE_GETADDRINFO
+    int err;
+
+    struct addrinfo hints = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    const struct addrinfo *aii;
+    struct addrinfo *ai;
+
+    hints.ai_family = address_family;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_socktype = SOCK_STREAM;
+    err = getaddrinfo(nodename, service, &hints, &ai);
+    if (err) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP,
+	       "cannot resolve (getaddrinfo) %s:%s: %s",
+	       nodename, service, gai_strerror(err));
+	return -1;
+    }
+
+    sock = -1;
+    errcause = "no addresses";
+    errno = 0;
+    for (aii = ai; aii; aii = aii->ai_next) {
+	sock = any_connect(aii->ai_family, aii->ai_socktype, aii->ai_protocol,
+			   aii->ai_addr, aii->ai_addrlen, &errcause);
+	if (sock >= 0)
+	    break;
+    }
+    freeaddrinfo(ai);
+
+    if (sock < 0) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP, "connecting to %s:%s: %s: %m",
+	       nodename, service, errcause);
+    }
+
+    return sock;
+#else
+    long port;
+
+    struct hostent *he;
+    struct sockaddr_in s_in;
+
+    char **ha;
+
+    if (address_family != 0 && address_family != AF_INET) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP,
+	       "unsupported address family %d", address_family);
+	return -1;
+    }
+
+    if (0 == get_long(service, &port)) {
+	struct servent *se;
+
+	se = getservbyname(service, "tcp");
+	if (NULL == se) {
+	    port = -1;
+	} else {
+	    port = ntohs(se->s_port);
+	}
+    } else {
+	if (port > 65535)
+	    port = -1;
+    }
+    if (port < 0) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP,
+	       "cannot get port for tcp service %s", service);
+	return -1;
+    }
+
+    he = gethostbyname(nodename);
+    if (!he) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP,
+	       "cannot resolve (gethostbyname) %s: %s",
+	       nodename, my_h_strerror(h_errno));
+	return -1;
+    }
+
+    sock = -1;
+    errcause = "no addresses";
+    errno = 0;
+    for (ha = he->h_addr_list; *ha; ha++) {
+	s_in.sin_family = AF_INET;
+	s_in.sin_port = htons(port);
+	memcpy(&s_in.sin_addr, *ha, he->h_length);
+	sock = any_connect(AF_INET, SOCK_STREAM, IPPROTO_TCP,
+			   (struct sockaddr *)&s_in, sizeof(s_in),
+			   &errcause);
+	if (sock >= 0)
+	    break;
+    }
+    endhostent();
+
+    if (sock < 0) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP, "connecting to %s:%s: %s: %m",
+	       nodename, service, errcause);
+    }
+
+    return sock;
+#endif
+}
+
 /*
  * connect to upstream nntp server
  *
@@ -170,115 +322,84 @@ extern struct state _res;
 int
 nntpconnect(const struct serverlist *upstream)
 {
-    struct hostent *hp;
-    static struct servent *sp;
-    struct servent sp_def;
-    struct sockaddr_in s_in;
-    int sock, reply;
-    static int i;
+    int sock, reply, infd;
     socklen_t optlen = sizeof(sendbuf);
     char *line;
+    char service[20];
 
-    memset((void *)&s_in, 0, sizeof(s_in));
     if (upstream->port == 0) {
-	sp = getservbyname("nntp", "tcp");
-	if (sp == NULL) {
-	    ln_log(LNLOG_SERR, LNLOG_CTOP, "unable to find service NNTP");
-	    return FALSE;
-	}
+	strcpy(service, "nntp");
     } else {
-	sp = &sp_def;
-	sp->s_port = htons(upstream->port);
+	str_ulong(service, upstream->port);
     }
-    /* Fetch the ip addresses of the given host. */
-    /* FIXME: not subject to timeout control. */
-    hp = gethostbyname(upstream->name);
-    if (hp) {
-	/* Try to make connection to each of the addresses in turn. */
-	for (i = 0; (char *)(hp->h_addr_list)[i]; i++) {
-	    int infd;
 
-	    s_in.sin_family = hp->h_addrtype;
-	    s_in.sin_port = sp->s_port;
-	    memcpy(&s_in.sin_addr, hp->h_addr_list[i], hp->h_length);
-	    sock = socket(AF_INET, SOCK_STREAM, 0);
-	    /* FIXME: place IP into buffer and log */
-	    if (sock < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot create socket: %m");
-		break;
-	    }
-	    if (setjmp(timeout) != 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "timeout connecting to %s",
-		       upstream->name);
-		(void)close(sock);
-		continue;
-	    }
-	    (void)signal(SIGALRM, timer);
-	    (void)alarm((unsigned)upstream->timeout);
-	    if (connect(sock, (struct sockaddr *)&s_in, sizeof(s_in)) < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot connect to %s: %m",
-		       upstream->name);
-		break;
-	    }
-	    (void)alarm((unsigned)0);
-	    nntpout = fdopen(sock, "w");
-	    if (nntpout == NULL)
-		break;
-	    infd = dup(sock);
-	    if (infd < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CSERVER, "cannot dup: %m");
-		break;
-	    }
-	    nntpin = fdopen(infd, "r");
-	    if (nntpin == NULL)
-		break;
-	    reply = newnntpreply(&line);
-	    if (line && strstr(line, "NewsCache")) {
-		/* NewsCache has a broken STAT implementation
-		   always returns 203 0 Message-ID
-		   breaking our upstream dupe detection */
-		stat_is_evil = 1;
-	    } else {
-		stat_is_evil = 0;
-	    }
-	    if (reply == 200 || reply == 201) {
-		/* FIXME: IPv6 */
-		ln_log(LNLOG_SINFO, LNLOG_CSERVER,
-		       "%s: connected to %s:%u, response: %d",
-		       upstream->name,
-		       inet_ntoa(s_in.sin_addr), htons(s_in.sin_port), reply);
-	    }
-	    if (line && strstr(line, "NNTPcache server V2.3")) {
-		/* NNTPcache 2.3.3 is still in widespread use, but it
-		   has Y2k bugs which have only been fixed in a beta
-		   version as of 2001-12-24. This 2.3 version is
-		   unsuitable for any use. */
-		ln_log(LNLOG_SERR, LNLOG_CSERVER,
-		       "%s: Server greeting \"%s\" hints to NNTPcache v2.3.x. "
-		       "This server has severe Y2k bugs which make it "
-		       "unsuitable for use with leafnode. "
-		       "Ask the news server administrator to update.",
-		       upstream->name, line);
-		nntpdisconnect();
-		return 0;
-	    }
-	    if (getsockopt(fileno(nntpout), SOL_SOCKET, SO_SNDBUF,
-			   (char *)&sendbuf, &optlen) == -1) {
-		ln_log(LNLOG_SERR, LNLOG_CSERVER,
-		       "%s: error in getsockopt: %m", upstream->name);
-		nntpdisconnect();
-		return 0;
-	    }
-	    ln_log(LNLOG_SINFO, LNLOG_CSERVER,
-		   "%s: TCP send buffer size is %ld", upstream->name, sendbuf);
-	    return reply;
-	}			/* end of IP-addresses for loop */
-    } else {
-	ln_log(LNLOG_SERR, LNLOG_CSERVER,
-	       "cannot resolve %s: %s", upstream->name, my_h_strerror(h_errno));
+    ln_log(LNLOG_SINFO, LNLOG_CSERVER, "%s: connecting to port %s",
+	   upstream->name, service);
+    sock = tcp_connect(upstream->name, service, PF_UNSPEC);
+    if (sock < 0)
+	return 0;
+
+    infd = dup(sock);
+    if (infd < 0) {
+	ln_log(LNLOG_SERR, LNLOG_CSERVER, "cannot dup(%d): %m", sock);
+	(void)close(sock);
+	return 0;
     }
-    return 0;
-}				/* end of connect function */
+
+    nntpout = fdopen(sock, "w");
+    if (nntpout == NULL) {
+	ln_log(LNLOG_SERR, LNLOG_CSERVER, "cannot fdopen(%d): %m", sock);
+	(void)close(sock);
+	return 0;
+    }
+
+    nntpin = fdopen(infd, "r");
+    if (nntpin == NULL) {
+	ln_log(LNLOG_SERR, LNLOG_CSERVER, "cannot fdopen(%d): %m", infd);
+	(void)fclose(nntpout);
+	(void)close(sock);
+	return 0;
+    }
+
+    reply = newnntpreply(&line);
+    if (line && strstr(line, "NewsCache")) {
+	/* NewsCache has a broken STAT implementation
+	   always returns 203 0 Message-ID
+	   breaking our upstream dupe detection */
+	stat_is_evil = 1;
+    } else {
+	stat_is_evil = 0;
+    }
+    if (reply == 200 || reply == 201) {
+	ln_log(LNLOG_SINFO, LNLOG_CSERVER,
+	       "%s: connected (%d), banner: \"%s\"",
+	       upstream->name, reply, line ? line : "(none)");
+    }
+    if (line && strstr(line, "NNTPcache server V2.3")) {
+	/* NNTPcache 2.3.3 is still in widespread use, but it
+	   has Y2k bugs which have only been fixed in a beta
+	   version as of 2001-12-24. This 2.3 version is
+	   unsuitable for any use since 2000-01-01. */
+	ln_log(LNLOG_SERR, LNLOG_CSERVER,
+	       "%s: Server greeting \"%s\" hints to NNTPcache v2.3.x. "
+	       "This server has severe Y2k bugs which make it "
+	       "unsuitable for use with leafnode. "
+	       "Ask the news server administrator to update.",
+	       upstream->name, line);
+	nntpdisconnect();
+	return 0;
+    }
+    if (getsockopt(fileno(nntpout), SOL_SOCKET, SO_SNDBUF,
+		   (char *)&sendbuf, &optlen) == -1) {
+	ln_log(LNLOG_SERR, LNLOG_CSERVER,
+	       "%s: error in getsockopt: %m", upstream->name);
+	nntpdisconnect();
+	return 0;
+    }
+    ln_log(LNLOG_SINFO, LNLOG_CSERVER,
+	   "%s: TCP send buffer size is %ld", upstream->name, sendbuf);
+    return reply;
+}
 
 /*
  * disconnect from upstream server
