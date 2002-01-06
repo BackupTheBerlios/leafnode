4d4d1553c71bd1640571b5eeffe7fd0fedb392cb
Matthias Andree <matthias.andree@gmx.de>
2002-01-06 01:07:32 +0100

    Add lclint annotations.

--- get.h
+++ get.h
@@ -1,6 +1,6 @@
 #ifndef GET_H
 #define GET_H
 /* (c) 2000 by Matthias Andree. */
-int get_long(const char *, long *);
-int get_ulong(const char *, unsigned long *);
+int get_long(const char *, /*@out@*/ long *);
+int get_ulong(const char *,  /*@out@*/ unsigned long *);
 #endif
