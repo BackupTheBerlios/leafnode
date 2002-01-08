12941ad2f700f9a2ca22057a099b1152fc86be74
Matthias Andree <matthias.andree@gmx.de>
2002-01-08 16:26:00 +0100

    Include Ralf Wildenhues' lclint annotations.

--- critmem.h
+++ critmem.h
@@ -3,14 +3,19 @@
 
 #include <sys/types.h>
 
-void *mycritmalloc(const char *f, long, size_t size, const char *message);
-void *mycritcalloc(const char *f, long, size_t size, const char *message);
-void *mycritrealloc(const char *f, long, void *a, size_t size,
-		    const char *message);
-char *mycritstrdup(const char *f, long, const char *, const char *message);
-#define critmalloc(a,b) mycritmalloc(__FILE__,__LINE__,a,b)
-#define critcalloc(a,b) mycritcalloc(__FILE__,__LINE__,a,b)
-#define critrealloc(a,b,c) mycritrealloc(__FILE__,__LINE__,a,b,c)
-#define critstrdup(a,b) mycritstrdup(__FILE__,__LINE__,a,b)
+/*@only@*//*@out@ */ void *mycritmalloc(const char *f, long, size_t size,
+					const char *message);
+/*@only@*/ void *mycritcalloc(const char *f, long, size_t size,
+			      const char *message);
+/*@only@*//*@out@ *//*@notnull@ */ void *mycritrealloc(const char *f, long,
+/*@null@ *//*@only@ *//*@out@ *//*@returned@ */
+						       void *a, size_t size,
+						       const char *message);
+/*@only@*/ char *mycritstrdup(const char *f, long, const char *,
+			      const char *message);
+#define critmalloc(a,b) mycritmalloc(__FILE__,(long)__LINE__,a,b)
+#define critcalloc(a,b) mycritcalloc(__FILE__,(long)__LINE__,a,b)
+#define critrealloc(a,b,c) mycritrealloc(__FILE__,(long)__LINE__,a,b,c)
+#define critstrdup(a,b) mycritstrdup(__FILE__,(long)__LINE__,a,b)
 
 #endif
