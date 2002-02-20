3caf72c819b558f94f7c8ee68332d0ad941f6f2d
Matthias Andree <matthias.andree@gmx.de>
2002-02-20 02:42:58 +0100

    Ralf Wildenhues: Suppress openlog in TESTMODE.

--- ln_log_open.c
+++ ln_log_open.c
@@ -9,10 +9,12 @@
 void
 ln_log_open(const char *ident)
 {
+#ifndef TESTMODE
 #ifdef HAVE_OLD_SYSLOG
     /* support legacy syslog interface */
     openlog(ident, LOG_PID);
 #else
     openlog(ident, LOG_PID | LOG_CONS, LOG_NEWS);
 #endif
+#endif /* not TESTMODE */
 }
