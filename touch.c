b51722085710fd0113a5d2e258efb5eb9d680dbb
Matthias Andree <matthias.andree@gmx.de>
2002-04-23 10:36:20 +0100

    Rename touch() to touch_truncate().

--- touch.c
+++ touch.c
@@ -8,7 +8,7 @@
 /** Create a file and make sure it has a current mtime. Note this file
     is not fsynced. */
 int
-touch(const char *name)
+touch_truncate(const char *name)
 {
     int fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0600);
     int r, e;
