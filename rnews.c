e47f95536b5824179245b41e4de2a2a0d5c2f7f3
Matthias Andree <matthias.andree@gmx.de>
2002-07-03 14:29:22 +0100

    Bugfix and revive rnews.c

--- rnews.c
+++ rnews.c
@@ -4,10 +4,12 @@
  * Written and copyrighted by Cornelius Krasel, January-April 1999
  * See README for restrictions on the use of this software.
  */
+
 #include "leafnode.h"
 #include "get.h"
 #include "critmem.h"
 #include "ln_log.h"
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <ctype.h>
@@ -28,9 +30,7 @@
 #include <dmalloc.h>
 #endif
 
-#define TEMPLATE "/tmp/rnews.XXXXXX"
 /* extern int optind; */
-int debug = 0;
 static int processfile(char *filename);
 static void
 usage(void)
@@ -49,25 +49,33 @@ usage(void)
  * return 1 if successful, 0 otherwise
  */
 static int
-fprocessbatch(FILE * f)
+fprocessbatch(FILE * f, char *firstline)
 {
     char *l;
     long bytes;
+    int ret;
 
-    while ((l = getaline(f))) {
+    while ((l = (firstline ? firstline : getaline(f)))) {
 	static const char tomatch[] = "#! rnews ";
+	firstline = NULL;
+	ln_log(LNLOG_SDEBUG, LNLOG_CARTICLE,
+		"fprocessbatch: read \"%s\"", l);
 
-	if (!strisprefix(l, tomatch)) {
-	    ln_log(LNLOG_SERR, LNLOG_CTOP,
-		   "expected `#! rnews', got `%-.40s[...]'", l);
+	if (!str_isprefix(l, tomatch)) {
+	    ln_log(LNLOG_SERR, LNLOG_CARTICLE,
+		   "expected `#! rnews', got %-.40s%s", l,
+		   strlen(l) > 40 ? "[...]" : "");
 	    goto pb_bail;
 	}
 	if (0 == get_long(l + strlen(tomatch), &bytes)) {
-	    ln_log(LNLOG_SERR, LNLOG_CTOP,
-		   "cannot extract article length from `%-.40s[...]'\n", l);
+	    ln_log(LNLOG_SERR, LNLOG_CARTICLE,
+		   "cannot extract article length from `%-.40s%s'\n", l,
+		   strlen(l) > 40 ? "[...]" : "");
 	    goto pb_bail;
 	}
-	store_stream(f, 0, NULL /* FIXME: filters */ , bytes);
+	ret = store_stream(f, 0, filter , bytes);
+	ln_log(LNLOG_SDEBUG, LNLOG_CARTICLE,
+			"store_stream returns %d", ret);
     }
     return 1;
   pb_bail:
@@ -78,105 +86,90 @@ fprocessbatch(FILE * f)
  * FILE pointer: ok
  */
 static FILE *
-decompresspipe(FILE * original, const char *compressor)
+decompresspipe(FILE *original, const char *const compressor[])
 {
+    /* plan: use a "feeder" process to read from original and pipe into
+     * the "exploder" process. (pipe #1)
+     * use an "exploder" process to decompress data, and read from its
+     * output the file (pipe #2).
+     * this function returns the end of pipe #2.
+     */
     pid_t pidfeeder, pidexploder;
-    int stat;
+    int feedpipe[2];
+    int explodepipe[2];
+    FILE *f;
+    char *argv[10];
+    size_t i;
+
+    for(i=0; i+1 < sizeof(argv)/sizeof(argv[0]) && compressor[i]; i++) {
+	argv[i] = critstrdup(compressor[i],
+		"decompresspipe");
+    }
+    argv[i] = 0;
+
+    if (pipe(feedpipe)) return NULL;
+    if (pipe(explodepipe)) return NULL;
 
     pidfeeder = fork();
     switch (pidfeeder) {
-    case -1:
-	return 0;
-    case 0:
-	pidexploder = fork();
-	switch (pidexploder) {
 	case -1:
-	    _exit(127);
-	case 0:		/* child */
-	default:		/* parent */
-	}
-	/* child */
+	    return 0;
+	case 0:
+	    /* child of feeder */
+	    pidexploder = fork();
+	    switch (pidexploder) {
+		case -1:
+		    _exit(127);
+		case 0:		/* child -- exploder */
+		    close(feedpipe[1]);
+		    close(explodepipe[0]);
+		    if (0 == close(0) && 0 == dup(feedpipe[0])) {
+			close(feedpipe[0]);
+			if (0 == close(1) && 1 == dup(explodepipe[1])) {
+			    close(explodepipe[1]);
+			    /* leaks argv */
+			    execve(compressor[0], argv, NULL);
+			    _exit(127);
+			}
+		    }
+		    _exit(1);
+		default:		/* parent of exploder */
+		    break;
+	    }
+	    /* feeder, stuff FILE into child */
+	    {
+		char buf[4096];
+		size_t r;
+		int rc = 0;
+		close(feedpipe[0]);
+		close(explodepipe[0]);
+		close(explodepipe[1]);
+
+		while(!feof(original) && (r = fread(buf, 1, sizeof(buf), original))) {
+		    if (write(feedpipe[1], buf, r)) {
+			rc = 1;
+			break;
+		    }
+		}
+
+		if (ferror(original)) {
+		    rc = 1;
+		}
+		close(feedpipe[1]);
+		wait(NULL);
+		for(i = 0; argv[i]; i++)
+		    free(argv[i]);
+		_exit(0);
+	    }
+	    break;
     default:
 	/* parent of feeder */
     }
-}
-
-/*
- * uncompress a file in filename
- * compressor must support gzip-style arguments and be able run as filter
- * return 1 if successful, 0 if not
- */
-static int
-old_uncompressfile(const char *filename, const char *compressor)
-{
-    pid_t pid;
-    int statloc;
-    char s[PATH_MAX + 1];	/* FIXME */
-
-    pid = fork();
-    switch (pid) {
-    case 0:
-	{
-	    int in, out;
-	    /* child process uncompresses stuff */
-	    snprintf(s, sizeof(s), ".tmp.%s", filename);	/* truncate */
-
-	    in = open(filename, O_RDWR);
-	    if (in < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot open %s: %m", filename);
-		_exit(1);
-	    }
-
-	    out = open(s, O_WRONLY | O_CREAT, (mode_t) 0600);
-	    if (out < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot open %s: %m", filename);
-		close(in);
-		_exit(1);
-	    }
-
-	    if (dup2(in, 0) < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot dup %d: %m", in);
-		close(in);
-		close(out);
-		_exit(1);
-	    }
-
-	    if (dup2(out, 1) < 0) {
-		ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot dup %d: %m", out);
-		close(in);
-		close(out);
-		_exit(1);
-	    }
-
-	    execl(compressor, compressor, "-c", "-d", NULL);
-	    /* if this is reached, calling gzip has failed */
-	    _exit(99);
-	}
-    case -1:
-	ln_log(LNLOG_SERR, LNLOG_CTOP, "unable to fork()\n");
-	return 1;
-    default:
-	break;
-    }
-
-    if (waitpid(pid, &statloc, 0) == -1) {
-	ln_log(LNLOG_SERR, LNLOG_CTOP,
-	       "Decompressing failed: waitpid returned error: %s\n",
-	       strerror(errno));
-    }
-
-    if (WIFEXITED(statloc) == 0) {
-	ln_log(LNLOG_SERR, LNLOG_CTOP,
-	       "Decompressing failed: child crashed.\n");
-	log_unlink(s);
-	return 0;
-    } else if (WEXITSTATUS(statloc)) {
-	ln_log(LNLOG_SERR, LNLOG_CTOP, "Decompressing failed, got %d.\n",
-	       WEXITSTATUS(statloc));
-	log_unlink(s);
-	return 0;
-    }
-    return log_rename(s, filename) ? 0 : 1;
+    close(explodepipe[1]);
+    close(feedpipe[0]);
+    close(feedpipe[1]);
+    f = fdopen(explodepipe[0], "r");
+    return f;
 }
 
 /*
@@ -187,27 +180,24 @@ static int
 fprocessfile(FILE * f)
 {
     char *l;
-    const char *tempfilename = 0;
     int i;
 
     const struct compressors {
 	const char *command;	/* read from batch */
-	const char *compressor;	/* command to run */
+	const char *compressor[4];/* command to run */
     } compressors[] = {
-	{
-	"cunbatch", GZIP}, {
-	"rnews", CAT}, {
-	"zunbatch", GZIP}
+	{ "cunbatch", { GZIP, "-c", "-d", NULL} },
+	{ "gunbatch", { GZIP, "-c", "-d", NULL} },
+	{ "zunbatch", { GZIP, "-c", "-d", NULL} },
     };
 
-#if 0
-    debug = 0;
     l = getaline(f);
-    debug = debugmode;
     if (!l || strlen(l) < 2) {
 	return 0;
     }
-    if (strisprefix(l, "#!")) {
+    if (str_isprefix(l, "#! rnews ")) {
+	    return fprocessbatch(f, l);
+    } else if (str_isprefix(l, "#!")) {
 	char *c;
 
 	c = l + 2;
@@ -215,29 +205,17 @@ fprocessfile(FILE * f)
 	for (i = 0;
 	     i < (long)(sizeof(compressors) / sizeof(struct compressors));
 	     i++) {
-	    if (strisprefix(c, compressors[i].command)) {
-		int j;
-		/* FIXME: fuck me */
-/*		tempfilename = copytotempfile(f); */
-		if (tempfilename) {
-		    if (!uncompressfile(tempfilename,
-					compressors[i].compressor)) {
+	    if (str_isprefix(c, compressors[i].command)) {
+		FILE *uc;
+
+		uc = decompresspipe(f, compressors[i].compressor);
+		if (uc == NULL)
 			return 0;
-		    }
-		    j = processbatch(tempfilename);
-		    if (j)
-			log_unlink(tempfilename);
-		} else {
-		    j = 0;
-		}
-		return j;
+		return fprocessbatch(uc, NULL);
 	    }
 	}
     }
     return 1;
-#else
-    return 0;
-#endif
 }
 
 /*
@@ -301,30 +279,46 @@ main(int argc, char *argv[])
     char *ptr;
     char option;
     struct stat st;
+    char conffile[PATH_MAX+1];
+    int err;
 
-    puts("this program cannot currently be used.");
-    abort();
+    if (((err = snprintf(conffile, sizeof(conffile), "%s/config", libdir)) < 0)
+	|| (err >= (int)sizeof(conffile)))
+	exit(EXIT_FAILURE);
 
     ln_log_open(argv[0]);
     if (!initvars(argv[0], 0))
 	exit(EXIT_FAILURE);
 
-
-    while ((option = getopt(argc, argv, "D:Vv")) != -1) {
-	if (!parseopt("rnews", option, NULL, NULL, 0)) {
-	    usage();
-	    exit(EXIT_FAILURE);
+    while ((option = getopt(argc, argv, "F:D:Vv")) != -1) {
+	if (parseopt("rnews", option, optarg, conffile, sizeof(conffile)))
+	    continue;
+	switch(option) {
+	    default:
+		usage();
+		exit(EXIT_FAILURE);
 	}
     }
 
-    debug = debugmode;
+    if (readconfig(conffile) != 0) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP, "Reading configuration failed: %m.\n");
+	exit(2);
+    }
+
     umask((mode_t) 077);
 
-    whoami();
-    if (lockfile_exists(FALSE, 0UL)) {
+    if (lockfile_exists(5UL)) {
 	fprintf(stderr, "%s: lockfile %s exists, abort\n", argv[0], lockfile);
 	exit(EXIT_FAILURE);
     }
+
+    if (filterfile && !readfilter(filterfile)) {
+	ln_log(LNLOG_SERR, LNLOG_CTOP,
+		"%s: Cannot read filterfile %s, aborting.",
+		argv[0], filterfile);
+	exit(EXIT_FAILURE);
+    }
+
     rereadactive();
     readlocalgroups();
     if (!argv[optind])
