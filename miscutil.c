e4ad76de9df3b322c3dbe3228ca7f2687d609b15
Matthias Andree <matthias.andree@gmx.de>
2002-09-09 23:30:06 +0100

    moved lookup and ihave into separate midutil.c

--- miscutil.c
+++ miscutil.c
@@ -428,63 +428,6 @@ is_dormant(const char *groupname)
     return  rbfind(groupname, rb_dormant) != 0;
 }
 
-/* no good but this server isn't going to be scalable so shut up */
-/*@dependent@*/ char *
-lookup(/*@null@*/ const char *msgid)
-{
-    static /*@null@*/ /*@owned@*/ char *name = NULL;
-    static unsigned int namelen = 0;
-    unsigned int r;
-    unsigned int i;
-    char *p;
-    const char *const myname = "lookup";
-
-    if (!msgid || !*msgid)
-	return NULL;
-
-    i = strlen(msgid) + strlen(spooldir) + 30;
-
-    if (!name) {
-	name = (char *)critmalloc(i, myname);
-	namelen = i;
-    } else if (i > namelen) {
-	name = (char *)critrealloc(name, i, myname);
-	namelen = i;
-    }
-
-    p = mastrcpy(name, spooldir);
-    p = mastrcpy(p, "/message.id/");
-    (void)mastrcpy(p + 4, msgid);
-    msgid_sanitize(p + 4);
-    r = msgid_hash(p + 4);
-    str_ulong0(p, r, 3);
-    p[3] = '/';
-    return name;
-}
-
-/** check if we already have an article with this message.id 
- \return 
-   - 0 if article not present or mid parameter NULL 
-   - 1 if article is already there */
-/*@falsewhennull@*/ int
-ihave(/*@null@*/ const char *mid
-/** Message-ID of article to check, may be NULL */ )
-{
-    const char *m = lookup(mid);
-    struct stat st;
-    if (m && (0 == stat(m, &st))) {
-	if (!S_ISREG(st.st_mode)) {
-	    ln_log(LNLOG_SWARNING, LNLOG_CARTICLE,
-		    "ihave(): article file %s is not a regular file (mode 0%o)",
-		   m,  st.st_mode);
-	    return 0;
-	} else {
-	    return 1;
-	}
-    }
-    return 0;
-}
-
 /** make directory and all parents with mode 0775
  * \bug exits the program with EXIT_FAILURE if it cannot create or change
  * into directory.
