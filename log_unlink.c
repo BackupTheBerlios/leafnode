5a0560c144e4fc9f732750596fc450a5bf8c7494
Matthias Andree <matthias.andree@gmx.de>
2002-09-14 00:23:57 +0100

    Forge 0 return code when ignore_enoent is set and ENOENT is seen.

--- log_unlink.c
+++ log_unlink.c
@@ -1,4 +1,4 @@
-/* (C) 2001 by Matthias Andree */
+/* (C) 2001 - 2002 by Matthias Andree */
 
 #include <unistd.h>
 #include "leafnode.h"
@@ -13,7 +13,9 @@ int
 log_unlink(const char *f, int ignore_enoent)
 {
     int r = unlink(f);
-    if (r < 0 && !(errno == ENOENT && ignore_enoent))
+    if (r < 0 && errno == ENOENT && ignore_enoent)
+	r = 0;
+    if (r < 0)
 	ln_log(LNLOG_SERR, LNLOG_CTOP, "cannot unlink %s: %m", f);
     return r;
 }
