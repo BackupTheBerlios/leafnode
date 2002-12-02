703f3d67b1550d9957f0eb4b97a350db40d71868
Matthias Andree <matthias.andree@gmx.de>
2002-12-02 21:15:45 +0100

    Kill autobootstrap. Use autoreconf -is (or -isf) instead.

--- autobootstrap.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-# This file has been written by Matthias Andree and is public domain.
-# All warranties are disclaimed. Use at your own risk.
-
-set -e
-aclocal
-autoheader
-automake -a
-autoconf
