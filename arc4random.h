add3d7396e92d4a307d5a2533fdfd9128a0d30a0
Matthias Andree <matthias.andree@gmx.de>
2003-01-27 23:29:03 +0100

    Portability fixes.

--- arc4random.h
+++ arc4random.h
@@ -1,6 +1,10 @@
 #ifndef ARC4RANDOM_H
 #define ARC4RANDOM_H
+
+#include "system.h"
+
 extern void arc4random_stir (void);
 extern void arc4random_addrandom (u_char *dat, int datlen);
-extern u_int32_t arc4random (void);
+extern uint32_t arc4random (void);
+
 #endif
