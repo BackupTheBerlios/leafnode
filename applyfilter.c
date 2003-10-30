8066ae54bd3aa488729e8457e825a20a9b288d66
Matthias Andree <matthias.andree@gmx.de>
2003-10-30 16:43:55 +0100

    Implement applyfilter -c and allow multiple newsgroup names to applyfilter. Implement applyfilter -c and allow multiple newsgroup names to applyfilter. Suggested by Reiner Steib.

--- applyfilter.c
+++ applyfilter.c
@@ -134,23 +134,130 @@ static void unfold(char *p)
     }
 }
 
+/* returns 0 for success */
+static int applyfilter(const char *name, struct newsgroup *g,
+	const struct filterlist *myfilter, int dryrun,
+	unsigned long *kept, unsigned long *deleted)
+{
+    static size_t lsize = MAXHEADERSIZE + 1;
+    static char *l;
+    struct stat st;
+    int score, fd;
+    struct utimbuf u;
+    unsigned long n;
+
+    l = (char *)critmalloc(lsize, "Space for article");
+
+    if (stat(name, &st)) {
+	ln_log(LNLOG_SNOTICE, LNLOG_CARTICLE,
+		"cannot stat file \"%s\" in newsgroup %s: %m",
+		g->name, name);
+	return 0;
+    }
+
+    /* remove truncated files */
+    if (st.st_size == 0)
+	score = TRUE;
+
+    if (!S_ISREG(st.st_mode)) {
+	ln_log(LNLOG_SNOTICE, LNLOG_CARTICLE,
+		"not a regular file in newsgroup %s: %s",
+		g->name, name);
+	return 0;
+    }
+
+    if((fd = open(name, O_RDONLY)) >= 0)
+    {
+	int ret;
+
+	/* read and unfold headers */
+	ret = readheaders(fd, name, &l, &lsize);
+	if (ret != -1)
+	    unfold(l);
+
+	switch (ret) {
+	    case 0:
+		score = killfilter(myfilter, l);
+		break;
+	    case -1:
+		score = TRUE;
+		break;
+	    case -2: /* article has no body */
+		if (delaybody_group(g->name))
+		    score = killfilter(myfilter, l);
+		else
+		    score = TRUE;
+		break;
+	    default:
+		/*@notreached@*/
+		score = FALSE;
+	}
+	close(fd);
+
+	if (score) {
+	    if (!dryrun) {
+		char *msgid = NULL;
+		if (strlen(l)) {
+		    msgid = mgetheader("Message-ID:", l);
+		    delete_article(msgid, "applyfilter", "filtered");
+		} else {
+		    unlink(name);
+		}
+	    } else { /* !dryrun */
+		if (verbose) {
+		    printf("%s would be deleted\n", name);
+		}
+	    }
+	    (*deleted)++;
+	} else {
+	    /* restore atime and mtime to keep texpire
+	     * functionality intact */
+	    u.actime = st.st_atime;
+	    u.modtime = st.st_mtime;
+	    utime(name, &u);
+	    (*kept) ++;
+	}
+
+	n = strtoul(name, NULL, 10);
+	if (n) {
+	    if (n < g->first)
+		g->first = n;
+	    if (n > g->last)
+		g->last = n;
+	}
+    } else /* if (fd = open) >= 0 */
+	ln_log(LNLOG_SERR, LNLOG_CARTICLE,
+		"could not open file \"%s\" in newsgroup %s\n",
+		name, g->name);
+    return 0;
+}
+
+
+static int
+setupfilter(struct filterlist **myfilter, const char *ng) {
+    if ((*myfilter = selectfilter(ng)) == NULL) {
+	printf("Nothing to filter -- no filter for %s found.\n", ng);
+	return 0;
+    }
+    return 1;
+}
+
 int
 main(int argc, char *argv[])
 {
     struct filterlist *myfilter;
     static const char c[] = "-\\|/";
-    int i, score, option, deleted, kept;
-    unsigned long n;
-    char *l, *conffile = NULL;
-    size_t lsize;
-    int fd;
+    int i, option;
+    unsigned long deleted, kept;
+    char *conffile = NULL;
     DIR *d;
     struct dirent *de;
-    struct stat st;
-    struct utimbuf u;
     struct newsgroup *g;
-    int err;
+    int err, savedir;
     int dryrun = 0;
+    int check = 0; /** if set, check if file given here would be filtered */
+
+    savedir=open(".", O_RDONLY);
 
     ln_log_open("applyfilter");
     if (!initvars(argv[0], 0)) {
@@ -158,16 +265,22 @@ main(int argc, char *argv[])
 	exit(EXIT_FAILURE);
     }
 
-    while ((option = getopt(argc, argv, GLOBALOPTS "n")) != -1) {
+    while ((option = getopt(argc, argv, GLOBALOPTS "nc")) != -1) {
 	if (parseopt("applyfilter", option, optarg, &conffile))
 	    continue;
 	switch (option) {
-	case 'n':
-	    dryrun = 1;
-	    break;
-	default:
-	    usage();
-	    exit(EXIT_FAILURE);
+	    case 'n':
+		dryrun = 1;
+		break;
+	    case 'c':
+		dryrun = 1; /* imply -n */
+		check = 1;
+		break;
+	    default:
+		usage();
+		/* fallthrough */
+	    case ':':
+		exit(EXIT_FAILURE);
 	}
     }
 
@@ -188,149 +301,106 @@ main(int argc, char *argv[])
 	exit(EXIT_FAILURE);
     }
 
-    if ((myfilter = selectfilter(argv[optind])) == NULL) {
-	printf("Nothing to filter -- no regexp for %s found.\n", argv[optind]);
-	exit(EXIT_SUCCESS);
-    }
-
     if (lockfile_exists(LOCKWAIT)) {
 	fprintf(stderr, "%s: lockfile %s exists, abort\n", argv[0], lockfile);
 	exit(EXIT_FAILURE);
     }
 
     rereadactive();
-    g = findgroup(argv[optind], active, -1);
-    if (!g) {
-	printf("Newsgroups %s not found in active file.\n", argv[optind]);
-	unlink(lockfile);
-	exit(EXIT_FAILURE);
-    }
-    g->first = ULONG_MAX;
-    if (!chdirgroup(g->name, FALSE)) {
-	printf("No such newsgroup: %s\n", g->name);
-	unlink(lockfile);
-	exit(EXIT_FAILURE);
-    }
-    if (!(d = opendir("."))) {
-	printf("Unable to open directory for newsgroup %s\n", g->name);
-	unlink(lockfile);
-	exit(EXIT_FAILURE);
-    }
-    i = 0;
-    deleted = 0;
-    kept = 0;
-    lsize = MAXHEADERSIZE + 1;
-    l = (char *)critmalloc(lsize, "Space for article");
-    ln_log(LNLOG_SINFO, LNLOG_CTOP, "Applying filters...");
-    while ((de = readdir(d)) != NULL) {
-	if (!isdigit((unsigned char)de->d_name[0])) {
-	    /* no need to stat file */
-	    continue;
-	}
-	switch (verbose) {
-	case 1:{
-		printf("%c", c[i % 4]);
-		fflush(stdout);
-		i++;
-		break;
+
+    if (check) {
+	kept = deleted = 0;
+	for(;optind<argc;optind++) {
+	    FILE *inf;
+	    char *ng;
+
+	    if (savedir >= 0) (void)fchdir(savedir);
+	    inf = fopen(argv[optind], "r");
+	    if (!inf) {
+		perror("applyfilter: fopen");
+		continue;
 	    }
-	case 2:{
-		printf("%s\n", de->d_name);
+
+	    ng = fgetheader(inf, "Newsgroups:", 1);
+	    fclose(inf);
+	    if (!ng) {
+		printf("File %s does not look like a news article, skipping;\nNewsgroups: header is missing.\n", argv[optind]);
+		continue;
 	    }
-	}
-
-	if (stat(de->d_name, &st)) {
-	    ln_log(LNLOG_SNOTICE, LNLOG_CARTICLE,
-		    "cannot stat file \"%s\" in newsgroup %s: %m",
-		    g->name, de->d_name);
-	    continue;
-	}
-
-	/* remove truncated files */
-	if (st.st_size == 0)
-	    score = TRUE;
-
-	if (!S_ISREG(st.st_mode)) {
-	    ln_log(LNLOG_SNOTICE, LNLOG_CARTICLE,
-		    "not a regular file in newsgroup %s: %s",
-		    g->name, de->d_name);
-	    continue;
-	}
-
-	if((fd = open(de->d_name, O_RDONLY)) >= 0)
-	{
-	    int ret;
-
-	    /* read and unfold headers */
-	    ret = readheaders(fd, de->d_name, &l, &lsize);
-	    if (ret != -1)
-		unfold(l);
-
-	    switch (ret) {
-	    case 0:
-		score = killfilter(myfilter, l);
-		break;
-	    case -1:
-		score = TRUE;
-		break;
-	    case -2: /* article has no body */
-		if (delaybody_group(g->name))
-		    score = killfilter(myfilter, l);
-		else
-		    score = TRUE;
-		break;
-	    default:
-		/*@notreached@*/
-		score = FALSE;
+	    g = findgroup(ng, active, -1);
+	    if (!g) {
+		printf("Newsgroups %s not found in active file.\n", ng);
+		free(ng);
+		continue;
 	    }
-	    close(fd);
 
-	    if (score) {
-		if (!dryrun) {
-		    char *msgid = NULL;
-		    if (strlen(l)) {
-			msgid = mgetheader("Message-ID:", l);
-			delete_article(msgid, "applyfilter", "filtered");
-		    } else {
-			unlink(de->d_name);
-		    }
-		} else { /* !dryrun */
-		    if (verbose) {
-			printf("%s would be deleted\n", de->d_name);
-		    }
+	    if (setupfilter(&myfilter, ng))
+		applyfilter(argv[optind], g, myfilter, dryrun, &kept, &deleted);
+	    free(ng);
+	}
+	printf("%lu articles %sdeleted, %lu kept.\n", deleted,
+		dryrun ? "would have been " : "",
+		kept);
+    } else {
+	for (;optind<argc;optind++) {
+	    kept = deleted = 0;
+	    g = findgroup(argv[optind], active, -1);
+	    if (!g) {
+		printf("Newsgroups %s not found in active file.\n", argv[optind]);
+		continue;
+	    }
+	    g->first = ULONG_MAX;
+	    if (!chdirgroup(g->name, FALSE)) {
+		printf("No articles for newsgroup: %s\n", g->name);
+		continue;
+	    }
+	    if (setupfilter(&myfilter, g->name) == 0)
+		continue;
+	    if (!(d = opendir("."))) {
+		printf("Unable to open directory for newsgroup %s\n", g->name);
+		unlink(lockfile);
+		continue;
+	    }
+	    i = 0;
+	    deleted = 0;
+	    kept = 0;
+	    ln_log(LNLOG_SINFO, LNLOG_CTOP, "Applying filters to %s...", g->name);
+	    while ((de = readdir(d)) != NULL) {
+		if (!isdigit((unsigned char)de->d_name[0])) {
+		    /* no need to stat file */
+		    continue;
 		}
-		deleted++;
-	    } else {
-		/* restore atime and mtime to keep texpire
-		 * functionality intact */
-		u.actime = st.st_atime;
-		u.modtime = st.st_mtime;
-		utime(de->d_name, &u);
-		kept++;
-	    }
+		switch (verbose) {
+		    case 1:{
+			       printf("%c", c[i % 4]);
+			       fflush(stdout);
+			       i++;
+			       break;
+			   }
+		    case 2:{
+			       printf("%s\n", de->d_name);
+			   }
+		}
 
-	    n = strtoul(de->d_name, NULL, 10);
-	    if (n) {
-		if (n < g->first)
-		    g->first = n;
-		if (n > g->last)
-		    g->last = n;
+		applyfilter(de->d_name, g, myfilter, dryrun, &kept, &deleted);
+	    } /* while readdir */
+	    closedir(d);
+	    if (g->first > g->last) {
+		/* group is empty */
+		g->first = g->last + 1;
 	    }
-	} else /* if (fd = open) >= 0 */
-	    ln_log(LNLOG_SERR, LNLOG_CARTICLE, "could not open file \"%s\" in newsgroup %s\n", de->d_name, g->name);
-    } /* while readdir */
-    closedir(d);
-    free(l);
-    if (g->first > g->last) {
-	/* group is empty */
-	g->first = g->last + 1;
+	    if (deleted) {
+		if (verbose)
+		    printf("Updating .overview file...");
+		getxover(1);
+	    }
+	    printf("%lu articles %sdeleted, %lu kept.\n", deleted,
+		    dryrun ? "would have been " : "",
+		    kept);
+	}
     }
     writeactive();
     unlink(lockfile);
-    printf("%d articles %sdeleted, %d kept.\n", deleted, dryrun ? "would have been " : "", kept);
-    if (verbose)
-	printf("Updating .overview file...");
-    getxover(1);
     if (verbose)
 	printf("Done.\n");
     freexover();
