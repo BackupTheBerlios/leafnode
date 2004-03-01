1b8d8a98803338aa7a518c3aaaedb2d23e421805
Matthias Andree <matthias.andree@gmx.de>
2004-03-01 14:35:48 +0100

    Local, site-specific release helpers stored in CVS.

---
+++ addpatches.pl
@@ -0,0 +1,80 @@
+#! /usr/bin/perl
+
+use strict;
+
+# prototypes
+sub gendiff($$\@);
+
+my $pat = 'leafnode-2.0.0.alpha';
+my $bookmark = 'SCRIPT MARKER #1 ';
+my $tar = '/bin/tar';
+my $diff = '/usr/bin/diff';
+my $gzip = '/usr/bin/gzip';
+
+chdir "$ENV{HOME}/public_html/leafnode/beta" or die "chdir: $!";
+my @tarballs = sort <$pat*.tar.bz2>;
+
+open F, "HEADER.html" or die "open: $!";
+my $lastver;
+my $havemarker;
+while(<F>) {
+    if (/$bookmark/) {
+	$havemarker++;
+    }
+    next unless $havemarker;
+    if (/upgrade-[^-]+-to-([^-]+).patch.gz/) {
+	$lastver = $1;
+	last;
+    }
+}
+close F;
+
+print "last version: $lastver\n";
+my $lastver_save = $lastver;
+@tarballs = grep { $_ gt "$lastver" }
+		map { s/$pat(.*)\.tar\.bz2/$1/; $_; } @tarballs;
+
+print "newer versions: ", join(" ", @tarballs), "\n";
+my $mostcurrent = $tarballs[$#tarballs];
+if (!@tarballs) { $mostcurrent = $lastver; }
+
+my @addtxt;
+foreach my $curver (@tarballs) {
+    exit 1 unless defined gendiff($lastver, $curver, @addtxt);
+    $lastver = $curver;
+}
+
+print "inserting:\n", join("", @addtxt);
+
+open F, "HEADER.html" or die "open: $!";
+my $state = 0;
+open O, ">HEADER.html.new" or die "open: $!";
+while (<F>) {
+    if ($state == 0) {
+	s/$lastver_save/$mostcurrent/g;
+    }
+    print O $_;
+    if (/$bookmark/) {
+	print O join("", @addtxt);
+	$state++;
+    }
+}
+close O or die "cannot close: $!";
+rename "HEADER.html.new", "HEADER.html" or die "rename: $!";
+close F;
+
+exit;
+
+sub gendiff($$\@) {
+    my ($last, $cur, $ary) = @_;
+
+    system ($tar, "-xjf", "$pat$last.tar.bz2") and return 1;
+    system ($tar, "-xjf", "$pat$cur.tar.bz2") and return;
+    system ("$diff -u $pat$last $pat$cur | $gzip -9cf >upgrade-$last-to-$cur.patch.gz");
+    system ("rm -rf $pat$last $pat$cur");
+    system ("gpg -ba --sign upgrade-$last-to-$cur.patch.gz");
+    unshift @$ary, "    <li><a href=\"upgrade-$last-to-$cur.patch.gz\">$last to\n"
+    . "        $cur</a> <a\n"
+    . "        href=\"upgrade-$last-to-$cur.patch.gz.asc\">[signature]</a></li>\n";
+    return 1;
+}
---
+++ release.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+
+set -e
+if test "x`cvs update | grep -v ^?`" != "x" ; then
+    echo "commit your changes to CVS first!"
+    exit 1
+fi
+dest=~/public_html/leafnode/beta/
+vers=`perl -n -l -e 'if (/AM_INIT_AUTOMAKE\(.*,\[?([^]]*)\]?\)/) { print "$1\n"; last; }' configure.ac`
+if [ x$QUICK != xquick ] ;then
+UPLOAD=yes
+if echo "$vers" | grep -E "devel|rc" ; then UPLOAD=no ; fi
+echo "test if make works"
+make -s
+echo "release for leafnode $vers"
+make -s dist
+cp -p leafnode-$vers.tar.bz2 $dest/
+#cp -p leafnode-$vers.lsm $dest/
+cp -p README $dest/leafnode-readme.txt
+#cp -p FAQ.{xml,html,txt,pdf} $dest/
+cp -p ChangeLog $dest/ChangeLog.txt
+cp -p NEWS TODO UPDATING $dest/
+if test ! -e leafnode-$vers.tar.bz2.asc -o ! leafnode-$vers.tar.bz2.asc -nt leafnode-$vers.tar.bz2 ; then
+    ( cd $dest ; gpg -ba --sign leafnode-$vers.tar.bz2 )
+fi
+vim $dest/RELEASE
+vim $dest/HEADER.html
+fi
+perl addpatches.pl
+synchome.sh &
+ssh krusty cvsup -L2 supfile &
+wait
+
+[ -f leafnode-ann.$vers ] && exit 0
+tmp=`mktemp leafnode-ann.$vers.XXXXXXXXXX`
+trap "rm -f $tmp" 0
+rm -f $tmp
+cat <<_EOF >$tmp
+Leafnode $vers is available from
+http://home.pages.de/~mandree/leafnode/beta/
+
+NEWS:
+_EOF
+
+sed '/^$/q;s/^- /+ /' NEWS >>$tmp
+gpg --clearsign $tmp
+
+nail -v -s "leafnode-$vers snapshot available" \
+    leafnode-list@dt.e-technik.uni-dortmund.de <$tmp.asc \
+&& touch leafnode-ann.$vers
+rm -f $tmp
