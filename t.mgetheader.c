6e9a8de57bddaed72db0d20c52d7ca2abad870eb
Matthias Andree <matthias.andree@gmx.de>
2004-05-10 08:11:40 +0100

    Add mgetheader check.

---
+++ t.mgetheader.c
@@ -0,0 +1,16 @@
+#include "leafnode.h"
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    const char *haystack = "X: y\nMessage-Id: teSt\n";
+    const char *needle = "Message-ID:";
+    char *res;
+    int rc = EXIT_SUCCESS;
+
+    res = mgetheader(needle, haystack);
+    if (!res || 0 != strcmp(res, "teSt"))
+	rc = EXIT_FAILURE;
+    if (res)
+	free(res);
+    exit(rc);
+}
