063382a24877ef494d09afd55591a12e5e545157
Matthias Andree <matthias.andree@gmx.de>
2004-12-13 12:57:02 +0100

    Add new changelog generator script.

---
+++ makechangelog.pl
@@ -0,0 +1,20 @@
+#! /usr/bin/env perl
+
+use strict;
+use HTTP::Date;
+
+my $re = '\S+\s+\S+\s+\d+\s+\d+:\d+:\d+\s+\S+\s+\d+\s+';
+my $cmd = "darcs changes -v";
+open F, "-|", "$cmd" or die "cannot run $cmd: $!";
+my $o = '';
+while(<F>) {
+    if (/^($re)/) {
+	my $t = HTTP::Date::str2time($1);
+	my $s = HTTP::Date::time2isoz($t);
+	$s =~ s/(....-..-..) ..:..:..Z/$1/;
+	s/^$re/$s  /;
+	if ($_ eq $o) { $_ = ''; }
+	else { $o = $_; }
+    }
+    print $_;
+}
