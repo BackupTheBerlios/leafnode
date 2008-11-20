710b8b141ddaaf7cf7003bb7020bfe4b81c89f79
Matthias Andree <matthias.andree@gmx.de>
2008-11-20 11:35:56 +0100

    Revamp output format to more closely resemble former.

--- makechangelog.sh
+++ makechangelog.sh
@@ -1,3 +1,17 @@
 #! /bin/sh
-git log --no-merges \
-| git name-rev --tags --stdin
+git log --decorate --stat --name-status --abbrev-commit --date=short \
+  --pretty=format:"=== %ad  %an <%ae>%n%s%n%b%n" -C -C \
+| perl -ne '
+    chomp;
+    $l = $_;
+    if (/^===/) {
+	if ($last ne $l) {
+	    print substr($l, 4), "\n";
+	}
+	$last = $l;
+	$chg = 1;
+    } else {
+	printf "%s%s\n", ($chg ? "\n  * " : "    "), $l;
+	$chg = 0;
+    }' \
+| uniq
