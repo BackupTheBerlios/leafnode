4ce7043296310b215f77091ccfe5e7e82c0d85dd
Matthias Andree <matthias.andree@gmx.de>
2008-11-20 11:40:32 +0100

    Fix spacing.

--- makechangelog.sh
+++ makechangelog.sh
@@ -10,8 +10,15 @@ git log --decorate --stat --name-status --abbrev-commit --date=short \
 	}
 	$last = $l;
 	$chg = 1;
+	$empty = 0;
     } else {
-	printf "%s%s\n", ($chg ? "\n  * " : "    "), $l;
+	if ($l) {
+	    printf "%s%s\n", ($chg ? "\n  * " : "    "), $l;
+	    $empty = 0;
+	} else {
+	    print "\n" unless $empty;
+	    $empty = 1;
+	}
 	$chg = 0;
     }' \
 | uniq
