9b39d6a7d407f7d25317cafeab0ff591c5aec785
Matthias Andree <matthias.andree@gmx.de>
2008-11-20 10:20:40 +0100

    Push to BerliOS.de as well.

--- export.sh
+++ export.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
 
-exec git push "$HOME"/public_html/leafnode/leafnode-2.git/
+a=0
+echo "===>  Pushing to own home directory"
+git push "$HOME"/public_html/leafnode/leafnode-2.git/ || a=1
+echo "===>  Pushing to BerliOS"
+git push ssh://m-a@git.berlios.de/gitroot/leafnode/ || a=1
+echo "===>  Finished."
+if [ $a -eq 0 ] ; then
+  echo "Success."
+else
+  echo "Error, see above for details!"
+fi
+exit $a
