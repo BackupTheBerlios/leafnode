ba29da48eb15826ffc9cd3a2c48a70ce0c0be07f
Matthias Andree <matthias.andree@gmx.de>
2009-04-06 22:22:08 +0200

    lsort: Fix error detection and reporting.
    If open fails, print file path and errno message.
    Check for write errors, and if there are any, abort writing
    and exit with EXIT_FAILURE.

--- lsort.c
+++ lsort.c
@@ -4,6 +4,10 @@
  * Written by Joerg Dietrich <joerg@dietrich.net>
  * (C) Copyright 1999
  *
+ * Bug fixes by Ralf Wildenhues, 2002
+ *
+ * Bug fixes by Matthias Andree, 2002, 2009
+ *
  * See file COPYING for restrictions on the use of this software.
  */
 
@@ -19,6 +23,7 @@
 #endif
 
 #include <stdio.h>
+#include <errno.h>
 
 static int
 lcomp(const void *a, const void *b)
@@ -34,13 +39,14 @@ main(void)
     size_t acount = 0;
     size_t i;
     FILE *f;
+    int err = 0;
     mastr *path = mastr_new(LN_PATH_MAX);
 
     (void)mastr_vcat(path, def_spooldir, "/leaf.node/groupinfo.old", NULL);
 
     f = fopen(mastr_str(path), "r");
     if (f == NULL) {
-	fprintf(stderr, "Error: Can't open groupinfo.old!\n");
+	fprintf(stderr, "Error: Can't open \"%s\": %s!\n", mastr_str(path), strerror(errno));
 	exit(EXIT_FAILURE);
     }
 
@@ -50,7 +56,7 @@ main(void)
     while ((l = getaline(f))) {
 	act = (char **)realloc(act, (acount + 1) * sizeof(char *));
 	if (act == NULL) {
-	    fprintf(stderr, "realloc returned NULL\n");
+	    fprintf(stderr, "out of memory: realloc returned NULL\n");
 	    exit(EXIT_FAILURE);
 	}
 	act[acount] = critstrdup(l, "lsort");
@@ -60,10 +66,16 @@ main(void)
 
     qsort(act, acount, sizeof(char *), &lcomp);
 
-    for (i = 0; i < acount; i++)
-	printf("%s\n", act[i]);
+    clearerr(stdout);
+    for (i = 0; i < acount; i++) {
+	int ok;
+	ok = printf("%s\n", act[i]);
+	if (ok < 0) { err = 1; break; }
+    }
 
     free(act);
     mastr_delete(path);
-    return 0;
+    fflush(stdout);
+    if (ferror(stdout)) err = 1;
+    return err ? EXIT_FAILURE : EXIT_SUCCESS;
 }
