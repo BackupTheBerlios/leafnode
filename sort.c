07edc88017596210a503848a6b19d5e4ada2ee15
Matthias Andree <matthias.andree@gmx.de>
2009-04-08 18:36:00 +0200

    Document sort.c.

--- sort.c
+++ sort.c
@@ -1,3 +1,10 @@
+/** \file
+ * Leafnode sort function wrapper, with comparison counting in debug 
+ * mode.
+ */
+/* (C) Copyright 2001 Matthias Andree
+ * with fixes 2002, 2003, 2004 and documentation 2009. 
+ * See COPYING for the license. */
 #include <sys/types.h>
 #include <sys/time.h>
 #include <unistd.h>
@@ -9,9 +16,15 @@
 
 #include "ln_log.h"
 
+/** Counter for number of comparisons, incremented by cmphook(). */
 static unsigned long compare;
+/** Variable for comparison function, passed to sort algorithms qsort() 
+ * and mergesort(), and set by my_sort().
+ */
 static int (*cmp) (const void *, const void *);
 
+/** Compare hook that increases the static counter compare.
+ * Used as wrapper around the real comparison function for evaluation. */
 static int
 cmphook(const void *a, const void *b)
 {
@@ -19,6 +32,19 @@ cmphook(const void *a, const void *b)
     return cmp(a, b);
 }
 
+/**
+ * Leafnode sort wrapper function, with mergesort-to-qsort fallback and 
+ * evaluation facility to count comparisons. This function will default 
+ * to trying mergesort() first, which can fail either because the data 
+ * elements are too small (this won't happen in leafnode) or because 
+ * there is not enough heap memory. In this case, this wrapper will fall 
+ * back to qsort(), which does not need additional memory except stack.
+ *
+ * The mergesort() used here is derived from BSD and has O(N) best-case 
+ * and O(N log N) worst-case behaviour; and it's good for pre-sorted 
+ * data, which is a common case in leafnode. See mergesort(3) for 
+ * details.
+ */
 void
 my_sort(void *base, size_t nmemb, size_t size,
       int (*compar) (const void *, const void *),
