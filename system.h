8c87df542004e83f0a8484d984dbff305a24e10a
Matthias Andree <matthias.andree@gmx.de>
2009-10-29 10:04:06 +0100

    Do not use sizeof() in preprocessor directive.

--- system.h
+++ system.h
@@ -20,9 +20,6 @@
 #include <inttypes.h>
 #elif HAVE_STDINT_H
 #include <stdint.h>
-#elif sizeof(unsigned long) == 4 && sizeof(unsigned char) == 1
-typedef unsigned long uint32_t;
-typedef unsigned char uint8_t;
 #else
 #error "I cannot figure how to define uint32_t and uint8_t."
 #endif
