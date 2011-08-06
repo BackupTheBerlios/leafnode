69a039e96f3f4cdfdeaad4415b2c652558ac3050
Matthias Andree <matthias.andree@gmx.de>
2011-08-07 01:09:51 +0200

    Fix regression: Push branches, too, not just tags.

--- export.sh
+++ export.sh
@@ -4,11 +4,11 @@ owndir="$HOME"/public_html/leafnode/leafnode-2.git/
 
 a=0
 echo "===>  Pushing to own home directory"
-git push --tags "$owndir" || a=1
+git push --tags "$owndir" : || a=1
 echo "===>  Repacking"
 ( cd "$owndir" && git repack --max-pack-size=1 --window=250 --depth=250 -d -l )
 echo "===>  Pushing to BerliOS"
-git push --tags ssh://m-a@git.berlios.de/gitroot/leafnode/ || a=1
+git push --tags ssh://m-a@git.berlios.de/gitroot/leafnode/ : || a=1
 echo "===>  Finished."
 if [ $a -eq 0 ] ; then
   echo "Success."
